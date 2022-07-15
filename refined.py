68c58fd1e45ba55097bec5e3f22a9f47208e5300
cgKyo <108032340+cgKyo@users.noreply.github.com>
2022-07-15 14:55:37 -0700

    Add files via upload

---
+++ refined.py
@@ -0,0 +1,38 @@
+
+
+print("Welcome to KYOS BIG DICK CALCULATOR BABY. What you trying to solve baby gurl?\n 1(Addition) 2(Subtraction) 3(Multiplication) 4(Division)")
+num = (input())
+
+if num == "1":
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number: "))
+    sum = num1 + num2
+    print("The Sum Is ", sum)
+
+    
+if num == "2":
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number: "))
+    sum = num1 - num2
+    print("The Difference Is ", sum)
+
+    
+
+if num == "3":
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number: "))
+    sum = num1 * num2
+    print("The Product Is ", sum)
+
+    
+
+if num == "4":
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number: "))
+    sum = num1 / num2
+    print("The Quotient Is ", sum)
+
+else:
+    print("Thats not a number in the list you silly billy goose.")
+    
+
